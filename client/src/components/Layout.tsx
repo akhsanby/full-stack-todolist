a2cf3291cfc2ec3abdc238ab9f864a35c0bf034c
akhsanby <akhsanby@gmail.com>
2024-01-16 13:35:32 +0700

    integrate logout function

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,13 +1,47 @@
+import axiosClient from "@/utils/api";
 import { ReactNode } from "react";
+import { destroyCookie } from "nookies";
+import { useRouter } from "next/router";
 
 type Props = {
   children: ReactNode;
+  jwtToken: string | undefined;
+  decodeToken: {
+    user_id: string;
+    username: string;
+  };
 };
 
-export default function Layout({ children }: Props) {
+export default function Layout({ children, jwtToken, decodeToken }: Props) {
+  const router = useRouter();
+  function handleLogout() {
+    axiosClient
+      .put(
+        "/api/user/logout",
+        {
+          username: decodeToken.username,
+          user_id: decodeToken.user_id,
+        },
+        {
+          headers: { Authorization: jwtToken },
+        }
+      )
+      .then((result) => {
+        if (result.data.status === "Logout") {
+          destroyCookie(null, "token");
+          router.replace("/");
+        }
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }
+
   return (
     <div className="w-screen h-full px-3 py-3">
-      <button className="btn btn-error btn-sm float-end">Logout</button>
+      <button className="btn btn-error btn-sm float-end" onClick={handleLogout}>
+        Logout
+      </button>
       {children}
     </div>
   );
