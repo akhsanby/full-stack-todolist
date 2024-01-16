5a1906d91b836cda0164447dc947216a40aeeb2c
akhsanby <akhsanby@gmail.com>
2024-01-16 10:42:21 +0700

    default datetime config

--- client/src/utils/moment.ts
+++ client/src/utils/moment.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 import "moment/locale/id.js";
 
-const dateTime = moment().format("lll");
+const dateTime = moment().locale("id").format("Do MMM YYYY, HH:mm");
 
 export default dateTime;
