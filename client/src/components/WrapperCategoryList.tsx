c6c39eef38acc8f2ae7966f2fda2d1afedf652e6
akhsanby <akhsanby@gmail.com>
2024-03-16 11:45:50 +0700

    implement drag category

--- client/src/components/WrapperCategoryList.tsx
+++ client/src/components/WrapperCategoryList.tsx
@@ -1,11 +1,14 @@
 import { badgeButton } from "@/utils/category-color";
-import { useTodoStore } from "@/utils/store";
 
 function WrapperCategoryList() {
+  function dragCategory(ev: any) {
+    ev.dataTransfer.setData("category", ev.target.getAttribute("data-category-name"));
+  }
+
   return (
     <div className="mb-3 flex flex-wrap gap-3">
       {badgeButton.map((btn, index) => (
-        <div key={index} className={`${btn.colorClass} !rounded cursor-grab select-none font-semibold`}>
+        <div draggable onDragStart={dragCategory} data-category-name={btn.name} key={index} className={`${btn.colorClass} !rounded select-none font-semibold`}>
           {btn.name}
         </div>
       ))}
