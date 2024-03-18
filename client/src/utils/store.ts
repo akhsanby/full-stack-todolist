e6ea41a397955026c02fe00938a9ad7000f76d67
akhsanby <akhsanby@gmail.com>
2024-03-18 19:45:12 +0700

    refactoring store

--- client/src/utils/store.ts
+++ client/src/utils/store.ts
@@ -1,79 +1,104 @@
 import { create } from "zustand";
-import { createTodoAPI, updateTodoAPI, removeTodoAPI } from "@/utils/api";
+import { Todos, Todo, TodoState } from "@/utils/types";
 
-type Todos = {
-  todo_id: string;
-  status: "todo" | "doing" | "done";
-  category: string[];
-  text: string;
-  user_id: string;
-  createdAt: string;
-}[];
-
-type Todo = {
-  todo_id: string;
-  status: "todo" | "doing" | "done";
-  category: string[];
-  text: string;
-  user_id: string;
-  createdAt: string;
-};
-
-interface TodoState {
-  todos: Todos;
-  setTodo: {
-    status?: "todo" | "doing" | "done";
-    category?: string[];
-    text?: string;
-    todo_id: string;
-    user_id: string;
-  };
-  storeTodos: (todos: Todos) => void;
-  createTodo: (todos: Todo) => void;
-  updateTodo: (todos: any) => void;
-  removeTodo: (todos: any) => void;
-}
-
-export const useTodoStore = create<TodoState>()((set) => ({
-  todos: [],
-  setTodo: {
-    status: undefined,
-    category: [],
-    text: "",
-    todo_id: "",
-    user_id: "",
+export const useTodoStore = create<TodoState>((set) => ({
+  todos: {
+    todo: [],
+    doing: [],
+    done: [],
   },
-  storeTodos: (data) => set(() => ({ todos: data })),
-  createTodo: (data) => {
-    createTodoAPI(data);
-    set((state: any) => ({ todos: [...state.todos, data] }));
-  },
-  updateTodo: (data: any) => {
-    updateTodoAPI(data);
+  moveTodo: (data: any) => {
     set((state: any) => {
-      const newArr = state.todos.filter((item: any) => {
-        if (item.todo_id === data.todo_id && data.status) {
-          item.status = data.status;
-        }
-        if (item.todo_id === data.todo_id && data.category) {
-          item.category = data.category;
-        }
-        if (item.todo_id === data.todo_id && data.text) {
-          item.text = data.text;
-        }
-        return item;
-      });
-      return { todos: newArr };
+      state.todos.todo = data.todo;
+      state.todos.doing = data.doing;
+      state.todos.done = data.done;
+      return state.todos;
     });
   },
-  removeTodo: (data: any) => {
-    removeTodoAPI({
-      todo_id: data.todo_id,
-      user_id: data.user_id,
+  storeTodos: (data: Todos) => {
+    set(() => {
+      const updatedTodos = {
+        todo: data.filter((todo) => todo.status === "todo"),
+        doing: data.filter((todo) => todo.status === "doing"),
+        done: data.filter((todo) => todo.status === "done"),
+      };
+      return { todos: updatedTodos };
     });
+  },
+  createTodo: (data: Todo, contentStatus: string) => {
     set((state: any) => {
-      const newArr = state.todos.filter((item: any) => item.todo_id !== data.todo_id);
-      return { todos: newArr };
+      if (contentStatus === "todo") {
+        state.todos.todo = [...state.todos.todo, data];
+      }
+      if (contentStatus === "doing") {
+        state.todos.doing = [...state.todos.doing, data];
+      }
+      if (contentStatus === "done") {
+        state.todos.done = [...state.todos.done, data];
+      }
+      console.log(state.todos);
+      return state.todos;
+    });
+  },
+  updateTodo: (data: any, contentStatus: string) => {
+    set((state: any) => {
+      if (contentStatus === "todo") {
+        state.todos.todo = state.todos.todo.map((item: Todo) => {
+          if (item.todo_id === data.todo_id && data.category) {
+            item.category = data.category;
+          }
+          if (item.todo_id === data.todo_id && data.text) {
+            item.text = data.text;
+          }
+          return item;
+        });
+      }
+      if (contentStatus === "doing") {
+        state.todos.doing = state.todos.doing.map((item: Todo) => {
+          if (item.todo_id === data.todo_id && data.category) {
+            item.category = data.category;
+          }
+          if (item.todo_id === data.todo_id && data.text) {
+            item.text = data.text;
+          }
+          return item;
+        });
+      }
+      if (contentStatus === "done") {
+        state.todos.done = state.todos.done.map((item: Todo) => {
+          if (item.todo_id === data.todo_id && data.category) {
+            item.category = data.category;
+          }
+          if (item.todo_id === data.todo_id && data.text) {
+            item.text = data.text;
+          }
+          return item;
+        });
+      }
+      return state.todos;
+    });
+  },
+  removeTodo: (data: any, contentStatus: string) => {
+    set((state: any) => {
+      if (contentStatus === "todo") {
+        state.todos.todo = state.todos.todo.filter((item: Todo) => item.todo_id !== data.todo_id);
+        state.todos.todo.forEach((item: Todo, index: number) => {
+          item.position = index;
+        });
+      }
+      if (contentStatus === "doing") {
+        state.todos.doing = state.todos.doing.filter((item: Todo) => item.todo_id !== data.todo_id);
+        state.todos.doing.forEach((item: Todo, index: number) => {
+          item.position = index;
+        });
+      }
+      if (contentStatus === "done") {
+        state.todos.done = state.todos.done.filter((item: Todo) => item.todo_id !== data.todo_id);
+        state.todos.done.forEach((item: Todo, index: number) => {
+          item.position = index;
+        });
+      }
+      return state.todos;
     });
   },
 }));
