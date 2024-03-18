b5871322797b2efd5b745d10c11f9e8a40cb4dfb
akhsanby <akhsanby@gmail.com>
2024-03-18 19:44:46 +0700

    remove unused api

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,5 +1,6 @@
 import nookies from "nookies";
 import axios from "axios";
+import { UpdateSyncTodoTypes } from "@/utils/types";
 
 const axiosClient = axios.create({
   baseURL: "http://localhost:8000",
@@ -8,70 +9,9 @@ const axiosClient = axios.create({
 
 export default axiosClient;
 
-type CreateTodoTypes = {
-  todo_id: string;
-  status: "todo" | "doing" | "done";
-  category: string[];
-  text: string;
-  user_id: string;
-  createdAt: string;
-};
-
-export async function createTodoAPI(body: CreateTodoTypes) {
+export function updateSyncTodoAPI(body: UpdateSyncTodoTypes, user_id: string) {
   const { token: jwtToken } = nookies.get();
-  return await axiosClient.post("/api/todo", body, {
-    headers: {
-      Authorization: jwtToken,
-    },
-  });
-}
-
-type UpdateTodoTypes = {
-  status?: "todo" | "doing" | "done" | string;
-  category?: string[];
-  todo_id: string;
-  text?: string;
-  user_id: string;
-};
-
-export async function updateTodoAPI(body: UpdateTodoTypes) {
-  const { token: jwtToken } = nookies.get();
-  return await axiosClient.patch("/api/todo", body, {
-    headers: {
-      Authorization: jwtToken,
-    },
-  });
-}
-
-type RemoveTodoTypes = {
-  todo_id: string;
-  user_id: string;
-};
-
-export function removeTodoAPI(body: RemoveTodoTypes) {
-  const { token: jwtToken } = nookies.get();
-  return axiosClient.delete("/api/todo", {
-    data: {
-      ...body,
-    },
-    headers: {
-      Authorization: jwtToken,
-    },
-  });
-}
-
-type MoveTodoTypes = {
-  status: {
-    from: "todo" | "doing" | "done";
-    to: "todo" | "doing" | "done";
-  };
-  todo_id: string;
-  user_id: string;
-};
-
-export function moveTodoAPI(body: MoveTodoTypes) {
-  const { token: jwtToken } = nookies.get();
-  return axiosClient.put("/api/todo/move", body, {
+  return axiosClient.patch(`/api/todo/sync/${user_id}`, body, {
     headers: {
       Authorization: jwtToken,
     },
