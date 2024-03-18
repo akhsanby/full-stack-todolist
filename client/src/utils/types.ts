1ddef3bcf53454f3d24e586c017840b2cfd3c715
akhsanby <akhsanby@gmail.com>
2024-03-18 19:45:45 +0700

    add new type

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -4,12 +4,70 @@ type Props = {
     user_id: string;
     username: string;
   };
+  resultTodos?: Todo[];
 };
 
 type EditingProps = {
-  todoId: string;
+  todo_id: string;
   isEdit: boolean;
   text: string;
 };
 
 export type { Props, EditingProps };
+
+export type CreateTodoTypes = {
+  todo_id: string;
+  position: number;
+  status: "todo" | "doing" | "done";
+  category: string[];
+  text: string;
+  user_id: string;
+  createdAt: string;
+};
+
+export type UpdateTodoTypes = {
+  status?: "todo" | "doing" | "done" | string;
+  category?: string[];
+  todo_id: string;
+  position?: number;
+  text?: string;
+  user_id: string;
+};
+
+export type UpdateSyncTodoTypes = {
+  todo_id: string;
+  status: "todo" | "doing" | "done" | string;
+  category: string[];
+  position: number;
+  text: string;
+  user_id: string;
+  createdAt: string;
+}[];
+
+export type RemoveTodoTypes = {
+  todo_id: string;
+  user_id: string;
+};
+
+export type Todo = {
+  todo_id: string;
+  position: number;
+  status: "todo" | "doing" | "done";
+  category: string[];
+  text: string;
+  user_id: string;
+  createdAt: string;
+};
+
+export interface TodoState {
+  todos: {
+    todo: Todo[];
+    doing: Todo[];
+    done: Todo[];
+  };
+  moveTodo: (todo: Todo) => void;
+  storeTodos: (todos: Todo[]) => void;
+  createTodo: (todos: Todo, contentStatus: string) => void;
+  updateTodo: (todos: any, contentStatus: string) => void;
+  removeTodo: (todos: any, contentStatus: string) => void;
+}
