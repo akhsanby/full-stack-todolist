d6ae2f6be5bb75bf409b36902156e6919d2d1c13
akhsanby <akhsanby@gmail.com>
2024-03-18 19:43:10 +0700

    padding top set 0

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -5,5 +5,5 @@ type Props = {
 };
 
 export default function Layout({ children }: Props) {
-  return <div className="w-screen h-full px-3 py-3">{children}</div>;
+  return <div className="w-screen h-full px-3 py-3 pt-0">{children}</div>;
 }
